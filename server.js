2e02bd483ec390b9be369f34de40d19ddb3d4559
Naldo de Andrade <106287645+francinaldodeandrade@users.noreply.github.com>
2023-10-28 19:41:11 -0300

    preparação do deploy

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const host_name = process.env.HOST_NAME
 const read = process.env.GET
 
 app.listen(port, (req, res)=>{
-console.log
-    (`servidor rodando no endereço: ${host_name}${port}${read}`);
+console.log(`servidor rodando neste local: ${host_name}${port}${read}`);
 })
 
